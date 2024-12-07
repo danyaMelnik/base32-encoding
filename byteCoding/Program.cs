3042fea98633c683b3ac8b7c6670af48d386cae9
webdev@asgltd.ru <webdev@asgltd.ru>
2024-12-07 22:15:41 +0300

    Add Program.cs

---
+++ byteCoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using byteCoding.Extensions;
+
+public static class Porgram {
+    public static void Main(String[] args)
+    {
+
+        byte[] inputBytes = new byte[3];
+
+        inputBytes[0] = 0b00001000;
+        inputBytes[1] = 0b01000010;
+        inputBytes[2] = 0b00000001;
+
+        string base32 = inputBytes.ToBase32();
+
+        byte[] fromBase32 = base32.FromBase32();
+    }
+
+}
